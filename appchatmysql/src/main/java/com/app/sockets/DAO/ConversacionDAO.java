5b26d3d692e23cb9bf6bf820048c43861abd9791
LUISMARRON <134035969+LUISMARRON@users.noreply.github.com>
2023-07-31 11:17:15 -0600

    SE AGREGAN CODIGOS Y VIDEO DE SOCKETS Y CHAT MYSQL

---
+++ appchatmysql/src/main/java/com/app/sockets/DAO/ConversacionDAO.java
@@ -0,0 +1,9 @@
+package com.app.sockets.DAO;
+
+public interface ConversacionDAO {
+
+    public void registrarConversacionA(String mensaje);
+
+    public void registrarConversacionB(String mensaje);
+
+}
